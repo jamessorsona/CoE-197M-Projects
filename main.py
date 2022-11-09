7930aabfb7b66066479f32d6eceaf1ce094434c7
James <jamessorsona@gmail.com>
2022-11-09 09:44:43 +0800

    new branch

---
+++ main.py
@@ -0,0 +1,22 @@
+""" 
+James Carlo E. Sorsona
+2019-01401
+CoE 197 M-THY
+Removing Projective Distortion on Images
+"""
+
+from tkinter import *
+from tkinter import filedialog
+import os
+from PIL import Image, ImageTk
+import numpy as np
+
+def main():
+    root = Tk()
+    root.title("Projective Distortion Remover")
+    root.geometry("1080x800")
+
+
+    root.mainloop()
+if __name__ == "__main__":
+    main()
